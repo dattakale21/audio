f10f02725232cdf542f4ce63944d82e6e8e6ed98
dattakale21 <dattakale2008@gmail.com>
2023-10-18 23:18:21 +0530

    some changes

--- app.py
+++ app.py
@@ -1,45 +1,186 @@
-from flask import Flask, request, jsonify
-import openai
+# language_codes = {
+#     "English": "en",
+#     "Spanish": "es",
+#     "French": "fr",
+#     "Hindi": "hi",
+#     "Marathi": "mr",
+#     "Afrikaans": "af",
+#     "Arabic": "ar",
+#     "Armenian": "hy",
+#     "Azerbaijani": "az",
+#     "Belarusian": "be",
+#     "Bosnian": "bs",
+#     "Bulgarian": "bg",
+#     "Catalan": "ca",
+#     "Chinese": "zh",
+#     "Croatian": "hr",
+#     "Czech": "cs",
+#     "Danish": "da",
+#     "Dutch": "nl",
+#     "Estonian": "et",
+#     "Finnish": "fi",
+#     "Galician": "gl",
+#     "German": "de",
+#     "Greek": "el",
+#     "Hebrew": "he",
+#     "Hungarian": "hu",
+#     "Icelandic": "is",
+#     "Indonesian": "id",
+#     "Italian": "it",
+#     "Japanese": "ja",
+#     "Kannada": "kn",
+#     "Kazakh": "kk",
+#     "Korean": "ko",
+#     "Latvian": "lv",
+#     "Lithuanian": "lt",
+#     "Macedonian": "mk",
+#     "Malay": "ms",
+#     "Maori": "mi",
+#     "Nepali": "ne",
+#     "Norwegian": "no",
+#     "Persian": "fa",
+#     "Polish": "pl",
+#     "Portuguese": "pt",
+#     "Romanian": "ro",
+#     "Russian": "ru",
+#     "Serbian": "sr",
+#     "Slovak": "sk",
+#     "Slovenian": "sl",
+#     "Swahili": "sw",
+#     "Swedish": "sv",
+#     "Tagalog": "tl",
+#     "Tamil": "ta",
+#     "Thai": "th",
+#     "Turkish": "tr",
+#     "Ukrainian": "uk",
+#     "Urdu": "ur",
+#     "Vietnamese": "vi",
+#     "Welsh": "cy",
+# }
+
+# def translate_audio():
+
+#         # with open(filename, "rb") as audio:
+#                     selected_language = input("Enter your language:")
+#             # print(f"Selected Language: {selected_language}")
+
+#                 # response = openai.Audio.transcribe("whisper-1", audio, lang="hi")
+
+
+#                     # text_to_translate = response["text"]
+#                     print(f"Selected Language: {selected_language}")
+#                     target_language = language_codes[selected_language]
+#                     print(f"Target Language: {target_language}")
+
+
+#             # except openai.error.OpenAIError as e:
+#             #     return f"OpenAI API error: {str(e)}"
+
+
+# Note: you need to be using OpenAI Python v0.27.0 for the code below to work
+from flask import Flask, render_template, request
+from langdetect import detect
+from googletrans import Translator, constants
+from googletrans import LANGUAGES
+from werkzeug.utils import secure_filename
+from gtts import gTTS
 import os
-
-app = Flask(__name__)
-
-# Set your OpenAI API key
-openai.api_key = "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"
-
-@app.route("/transcribe-and-translate", methods=["POST"])
-def transcribe_and_translate():
-    try:
-        target_language = request.form["targetLanguage"]
-        audio_file = request.files["audioFile"]
-
-        # Transcribe the audio file using OpenAI GPT-3
-        with open("audio.mp3", "wb") as file:
-            file.write(audio_file.read())
-
-        with open("audio.mp3", "rb") as file:
-            response = openai.Completion.create(
-                engine="davinci",
-                audio=file,
-                prompt="Transcribe the following audio:",
-                max_tokens=200
-            )
-
-        # Extract the transcription from the response
-        transcription_result = response.choices[0].text.strip()
-
-        return jsonify({
-            "transcriptionResult": transcription_result,
-        })
-
-    except Exception as e:
-        return jsonify({"error": str(e)})
-
-if __name__ == "__main__":
-    app.run(debug=True)
+import openai
+import pyttsx3
+import codecs
+from pprint import pprint
 
 
-#  "express": "^4.18.2",
-#     "multer": "^1.4.5-lts.1",
-#     "openai": "^4.12.0",
-#     "whisper"
+language_codes = {
+    "English": "en",
+    "Spanish": "es",
+    "French": "fr",
+    "Hindi": "hi",
+    "Marathi": "mr",
+    "Afrikaans": "af",
+    "Arabic": "ar",
+    "Armenian": "hy",
+    "Azerbaijani": "az",
+    "Belarusian": "be",
+    "Bosnian": "bs",
+    "Bulgarian": "bg",
+    "Catalan": "ca",
+    "Chinese": "zh",
+    "Croatian": "hr",
+    "Czech": "cs",
+    "Danish": "da",
+    "Dutch": "nl",
+    "Estonian": "et",
+    "Finnish": "fi",
+    "Galician": "gl",
+    "German": "de",
+    "Greek": "el",
+    "Hebrew": "he",
+    "Hungarian": "hu",
+    "Icelandic": "is",
+    "Indonesian": "id",
+    "Italian": "it",
+    "Japanese": "ja",
+    "Kannada": "kn",
+    "Kazakh": "kk",
+    "Korean": "ko",
+    "Latvian": "lv",
+    "Lithuanian": "lt",
+    "Macedonian": "mk",
+    "Malay": "ms",
+    "Maori": "mi",
+    "Nepali": "ne",
+    "Norwegian": "no",
+    "Persian": "fa",
+    "Polish": "pl",
+    "Portuguese": "pt",
+    "Romanian": "ro",
+    "Russian": "ru",
+    "Serbian": "sr",
+    "Slovak": "sk",
+    "Slovenian": "sl",
+    "Swahili": "sw",
+    "Swedish": "sv",
+    "Tagalog": "tl",
+    "Tamil": "ta",
+    "Thai": "th",
+    "Turkish": "tr",
+    "Ukrainian": "uk",
+    "Urdu": "ur",
+    "Vietnamese": "vi",
+    "Welsh": "cy",
+}
+
+print("hi")
+def speakText():
+    text = document.getElementsByName("transcribed_text")[0].value
+    engine = pyttsx3.init()
+    engine.say(text)
+    engine.runAndWait()
+
+# print(translation.text)
+
+# print(transcript)
+# print(tt)
+# from flask import Flask, render_template, request
+# from langdetect import detect
+# from googletrans import Translator, constants
+# from googletrans import LANGUAGES
+# from werkzeug.utils import secure_filename
+# from gtts import gTTS
+# import os
+# import openai
+# import codecs
+# from pprint import pprint
+
+# translator = Translator()
+# translation = translator.translate("எத்தனை கவிங்கன் எழுதி பார்த்துக்கொண்டான் காதல் தீர்ந்து போகல எத்தனை னடிக்கன் நடிச்சு பார்த்துக்கொண்டான் காதல் போறே அடிக்கல எத்தனை காதல் உத்தங்கள் நடந்தும், காதல் தோல்க்காமல் இருக்கு எத்தனை கண்ணீர் சிந்தியும் காதல் ரொம்ப சுகமாய் இருக்கு இந்த காதல் இல்லையே, மனிதன் யாவரும் இருகமாக நேரும் ",dest='en')
+# print(f"{translation.origin} ({translation.src}) --> {translation.text} ({translation.dest})")
+
+# if __name__ == '__main__':
+#     audio_path = "audio3.mp3"  # Provide the path to your audio file
+#     try:
+#         result = transcribe_audio(audio_path)
+#         print(result)
+#     except Exception as e:
+#         print(f"Error: {e}")
--- index.py
+++ index.py
@@ -1,23 +1,134 @@
 from flask import Flask, render_template, request
-from langdetect import detect
-from googletrans import Translator
+from langdetect import detect,detect_langs
+from googletrans import Translator, constants
+from googletrans import LANGUAGES
 from werkzeug.utils import secure_filename
 from gtts import gTTS
 import os
 import openai
+import codecs
+from pprint import pprint
 
 app = Flask(__name__)
 
-openai.api_key = "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"
+openai.api_key = "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"
 
+language_codes = {
+    "English": "en",
+    "Spanish": "es",
+    "French": "fr",
+    "Hindi": "hi",
+    "Marathi": "mr",
+    "Afrikaans": "af",
+    "Arabic": "ar",
+    "Armenian": "hy",
+    "Azerbaijani": "az",
+    "Belarusian": "be",
+    "Bosnian": "bs",
+    "Bulgarian": "bg",
+    "Catalan": "ca",
+    "Chinese": "zh",
+    "Croatian": "hr",
+    "Czech": "cs",
+    "Danish": "da",
+    "Dutch": "nl",
+    "Estonian": "et",
+    "Finnish": "fi",
+    "Galician": "gl",
+    "German": "de",
+    "Greek": "el",
+    "Hebrew": "he",
+    "Hungarian": "hu",
+    "Icelandic": "is",
+    "Indonesian": "id",
+    "Italian": "it",
+    "Japanese": "ja",
+    "Kannada": "kn",
+    "Kazakh": "kk",
+    "Korean": "ko",
+    "Latvian": "lv",
+    "Lithuanian": "lt",
+    "Macedonian": "mk",
+    "Malay": "ms",
+    "Maori": "mi",
+    "Nepali": "ne",
+    "Norwegian": "no",
+    "Persian": "fa",
+    "Polish": "pl",
+    "Portuguese": "pt",
+    "Romanian": "ro",
+    "Russian": "ru",
+    "Serbian": "sr",
+    "Slovak": "sk",
+    "Slovenian": "sl",
+    "Swahili": "sw",
+    "Swedish": "sv",
+    "Tagalog": "tl",
+    "Tamil": "ta",
+    "Thai": "th",
+    "Turkish": "tr",
+    "Ukrainian": "uk",
+    "Urdu": "ur",
+    "Vietnamese": "vi",
+    "Welsh": "cy",
+    
+}
+          
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
+    if request.method == "POST":
+        if "transcribe" in request.form:
+            return render_template("transcribe.html")
+        elif "translate" in request.form:
+            return render_template("translate.html")
     return render_template("index.html")
 
 
-@app.route("/transcribe", methods=["GET", "POST"])
+@app.route("/transcribe", methods=["POST"])
 def transcribe_audio():
+    audio_file = request.files["file"]
+    if audio_file.filename.split(".")[-1] not in [
+        "flac",
+        "m4a",
+        "mp3",
+        "mp4",
+        "mpeg",
+        "mpga",
+        "oga",
+        "ogg",
+        "wav",
+        "webm",
+    ]:
+        return "Invalid file format. Supported formats: flac, m4a, mp3, mp4, mpeg, mpga, oga, ogg, wav, webm"
+
+    filename = secure_filename(audio_file.filename)  # Secure the filename before saving
+    audio_file.save(filename)
+    with open(filename, "rb") as audio:
+        transcript = openai.Audio.transcribe("whisper-1", audio)
+        if isinstance(transcript, dict) and "text" in transcript:
+            decoded_text = transcript["text"]
+            try:
+                detected_language_code = detect_langs(decoded_text)[0].lang
+                detected_language = LANGUAGES.get(detected_language_code, "Unknown")
+                detected_language = detected_language.capitalize()
+            except IndexError:
+                detected_language = "Unknown Language"
+            print("Detected language:", detected_language)
+            print("Decoded Text:", decoded_text)
+            return render_template("transcribe.html", decoded_text=decoded_text, detected_language=detected_language)
+
+        elif decoded_text is not None:  # Check if the transcript exists
+            return decoded_text
+
+        else:
+            print("Transcription not found or incorrect format.")
+    # return render_template("tra.html")
+
+
+
+@app.route("/translate", methods=["GET", "POST"])
+def translate_audio():
     if request.method == "POST":
         audio_file = request.files["file"]
         if audio_file.filename.split(".")[-1] not in [
@@ -34,40 +145,40 @@ def transcribe_audio():
         ]:
             return "Invalid file format. Supported formats: flac, m4a, mp3, mp4, mpeg, mpga, oga, ogg, wav, webm"
 
-        filename = secure_filename(audio_file.filename)  # Secure the filename before saving
+        filename = secure_filename(audio_file.filename)
         audio_file.save(filename)
+
         with open(filename, "rb") as audio:
-            selected_language = request.form.get("language")
-            transcript = openai.Audio.translate("whisper-1", audio, target_language=selected_language)
+                selected_language = request.form.get("language")
+                print(f"Selected Language: {selected_language}")
 
-            # Convert the transcript to a string
-            transcript = str(transcript)
+            # try:
+                response = openai.Audio.transcribe("whisper-1", audio)
+                if isinstance(response, dict) and "text" in response:
+                    decoded_text = response["text"]
+                    try:
+                        detected_language_code = detect_langs(decoded_text)[0].lang
+                        detected_language = LANGUAGES.get(detected_language_code, "Unknown")
+                        detected_language = detected_language.capitalize()
+                    except IndexError:
+                        detected_language = "Unknown Language"
+                    translator = Translator()
+                    translated_text = translator.translate(decoded_text, dest=selected_language).text
+                    print("Detected language:", detected_language)
+                    print("Decoded Text:", decoded_text)
+                    return render_template("translate.html", decoded_text=translated_text, detected_language=detected_language)
 
-            # Perform language detection on the string format of the transcript
-            detected_language = detect(transcript)
+                else:
+                    return "Transcription failed or returned no data."
 
-            # Further processing or rendering logic
-            parsed_transcript = transcript  # Set as the parsed transcript
-            parsed_transcript = (
-                parsed_transcript.replace(' , "text": "', "").replace('" ,', "")
-            )  # Parsing the transcript text
-            return render_template(
-                "transcript.html",
-                transcript=parsed_transcript,
-                detected_language=detected_language,
-                audio_language=detected_language,
-            )
+            # except openai.error.OpenAIError as e:
+            #     return f"OpenAI API error: {str(e)}"
+            # except Exception as e:
+            #     return f"Error: {str(e)}"
 
+                if selected_language not in language_codes.keys():
+                    return "Invalid language selected."
 
-
-@app.route("/listen", methods=["GET", "POST"])
-def listen_transcript():
-    if request.method == "POST":
-        transcript = request.form["transcript"]
-        tts = gTTS(transcript)
-        tts.save("transcript.mp3")
-        os.system("start transcript.mp3")
-        return "Playing the transcript..."
 
 
 if __name__ == "__main__":
